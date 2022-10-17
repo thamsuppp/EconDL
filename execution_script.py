7fb0e02aecada034ee1e9bd313da788823a4d56a
Isaac Tham <isaactham2@hotmail.com>
2022-10-17 18:52:43 -0400

    Updated execution_script.py to reflect new instance organization

--- execution_script.py
+++ execution_script.py
@@ -1,6 +1,9 @@
 import torch
 import sys
+import random
 import os
+import json
+import time
 from EconDL.Run import Run
 from EconDL.Evaluation import Evaluation
 
@@ -9,46 +12,57 @@ print('Device', device)
 
 # Experiment name is the command-line argument
 run_name = sys.argv[1]
-num_experiments = int(sys.argv[2])
+num_repeats = int(sys.argv[2])
 
 folder_path = f'results/{run_name}'
 if os.path.isdir(folder_path) == False:
+  # Wait for a random number of seconds to avoid collision
+  time.sleep(random.randint(0, 10))
   os.mkdir(folder_path)
   print(f'Folder made at {folder_path}')
 else:
   print(f'Folder at {folder_path} already exists')
 
-# Instantiate the Run:
-# - Creates a new folder
-# - Reads the run's configuration json file
-# - Instantiates the experiments with the corresponding nn_hyps
-# - Loads the data into the RunObj
+instance_id = int(os.environ.get('SLURM_ARRAY_TASK_ID')) - 1
 
-task_id = int(os.environ.get('SLURM_ARRAY_TASK_ID')) - 1
+# Logic that calculates the number of experiment instances to instantiate
+with open(f'exp_config/{run_name}.json', 'r') as f:
+  all_params = json.load(f)
+num_experiments = len(all_params['nn_hyps'])
 
-# If we are doing this in parallel, then we pass in the job_id parameter here
+test_size = all_params['run_params']['test_size']
 
-experiment_id = task_id % num_experiments
-repeat = int(task_id / num_experiments)
+num_instances = 0
+instance_exp_mapping = {}
 
-print(f'Task ID {task_id}, Experiment {experiment_id}, repeat {repeat}')
-# Train the specific experiment_id and speciifc repeat_id
-RunObj = Run(run_name, device, experiment_id = experiment_id, job_id = repeat)
+for exp in range(num_experiments):
+  if all_params['nn_hyps'][exp]['reestim_params']['reestim'] == False:
+    for repeat in range(num_repeats):
+      instance_exp_mapping[num_instances] = {'exp': exp, 'reestim': 0, 'repeat': repeat}
+      num_instances += 1
+
+  else:
+    num_reestims = int(test_size / all_params['nn_hyps'][exp]['reestim_params']['reestimation_window'])
+    for reestim in range(num_reestims):
+      for repeat in range(num_repeats):
+        instance_exp_mapping[num_instances] = {'exp': exp, 'reestim': reestim, 'repeat': repeat}
+        num_instances += 1
+
+print(f'# Experiments: {num_experiments}, # Instances: {num_instances}')
+print(f'Instance to experiment mapping: {instance_exp_mapping}')
+
+
+# Run the experiment that is specified by this instance
+experiment_id = instance_exp_mapping[instance_id]['exp']
+reestim = instance_exp_mapping[instance_id]['reestim']
+repeat = instance_exp_mapping[instance_id]['repeat']
+
+print(f'Instance {instance_id}, {instance_exp_mapping[instance_id]}')
+RunObj = Run(run_name, device, experiment_id = experiment_id, job_id = repeat, reestim_id = reestim)
 RunObj.train_all()
 
 if experiment_id == 0: # Oly train the ML experiments once per across all experiments (only needed once)
   RunObj.train_ml_experiments()
-
-
-### Evaluating
-# If we are doing this in parallel, then we pass in the job_id parameter here
-# RunObj = Run(run_name, device)
-
-# EvaluationObj = Evaluation(RunObj)
-# EvaluationObj.Run.compute_conditional_irfs()
-
-# print(EvaluationObj.check_results_sizes())
-# EvaluationObj.plot_all()
 
 
 '''
